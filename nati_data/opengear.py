bd759e9a3ecdd878647fd32d7d4094021d9f37fc
Joshua James <josh@unobi.com>
2025-04-12 08:01:06 -0400

    Created Opengear module.

---
+++ nati_data/opengear.py
@@ -0,0 +1,11 @@
+"""
+Module for operating with Opengear Lighthouse (v3.7) and Operations Manager (v2.2) devices.
+
+Documentation:
+https://ftp.opengear.com/download/lighthouse_software/current/documentation/og-rest-api-specification-v3-7.html
+
+https://ftp.opengear.com/download/opengear_appliances/OM/archive/19.Q4.0/doc/og-rest-api-specification-v2-2-ngcs.html
+"""
+
+import json
+
